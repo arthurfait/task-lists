54447a6ae9bf13b3b2920aa4b617cf05617e32ba
Nesterov Sergey <Menestrel.me@gmail.com>
2012-01-06 18:35:12 +0200

    Add headers file

---
+++ headers.h
@@ -0,0 +1,20 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+// standart includes
+
+#include <list>
+#include <boost/shared_ptr.hpp>
+#include <QMainWindow>
+#include <QtGui/QApplication>
+
+using namespace std;
+using namespace boost;
+
+// my classes
+#include "listoftask.h"
+#include "ui_mainwindow.h"
+#include "mainwindow.h"
+
+
+#endif // HEADERS_H
