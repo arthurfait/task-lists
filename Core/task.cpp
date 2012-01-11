464aff7da70c87fa4e634db1ad50a21ba988a52f
Nesterov Sergey <Menestrel.me@gmail.com>
2012-01-11 16:42:22 +0200

    Add parametres for task.

--- Core/task.cpp
+++ Core/task.cpp
@@ -1,5 +1,56 @@
 #include "core-headers.h"
 
-Task::Task()
+Task::Task(string name, int complexity)
 {
+    setName(name);
+    setComplexity(complexity);
+    setProgress(false);
+}
+
+bool Task::SetComplexity(int complexity)
+{
+    //complexity range 1..10
+    int minComplexity = 1;
+    int maxComplexity = 10;
+
+    if (complexity >= minComplexity && complexity <= maxComplexity)
+    {
+        this->complexity = complexity;
+        return true;
+    }
+    else
+        return false;
+}
+
+const int Task::GetComplexity()
+{
+    return (const int) complexity;
+}
+
+void Task::SetName(string name)
+{
+    this->name = name;
+}
+
+const string Task::GetName()
+{
+    return (const string) name;
+}
+
+bool Task::SetProgress(bool progress)
+{
+    this->progress = progress;
+}
+
+const bool Task::GetProgress()
+{
+    return (const bool) progress;
+}
+
+Task::~Task()
+{
+    cout<<"Task deleted."<<endl;
+    cout<<"Name - "<<name<<endl;
+    cout<<"Completixy - "<<complexity<<endl;
+    cout<<"Progress - "<<progress<<endl;
 }
--- Core/task.h
+++ Core/task.h
@@ -6,8 +6,18 @@
 class Task
 {
 public:
-    Task();
-
+    Task(string name, int complexity);
+    ~Task();
+    const int GetComplexity();
+    bool SetComplexity(int complexity);
+    const string GetName();
+    void SetName(string name);
+    const bool GetProgress();
+    bool SetProgress(bool progress);
+private:
+    int complexity;
+    string name;
+    bool progress;
 };
 
 #endif // TASK_H
